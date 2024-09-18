dccf8169dcc1a0f86494d982af544a25c212a623
aya12sh <you@example.com>
2024-09-18 22:26:09 +0300

    update: fixing and start responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-console.log("i am ready")
+const menuButton = document.querySelector('.po-menu__container')
+const megaMenu = document.querySelector('.po-mega_menu')
+let open = false;
+
+
+menuButton.addEventListener('click', () => {
+open = !open
+console.log(open)
+if (open)  {
+  megaMenu.style.transform = "translateY(0)"
+  menuButton.style.transform = "rotate(90deg)"
+} else {
+  megaMenu.style.transform = "translateY(-400px)"
+  menuButton.style.transform = "rotate(0)"
+
+}
+})
