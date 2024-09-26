8e7573429882c3ece98ee8e31f2c49af33eac367
aya12sh <you@example.com>
2024-09-26 11:58:14 +0300

    update: finsh v0

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const menuButton = document.querySelector('.po-menu__container')
+const menuButton = document.querySelector('.po-menu__btn')
 const megaMenu = document.querySelector('.po-mega_menu')
 let open = false;
 
@@ -9,9 +9,11 @@ console.log(open)
 if (open)  {
   megaMenu.style.transform = "translateY(0)"
   menuButton.style.transform = "rotate(90deg)"
+  megaMenu.style.display= "block"
 } else {
   megaMenu.style.transform = "translateY(-400px)"
   menuButton.style.transform = "rotate(0)"
+  megaMenu.style.display= "none"
 
 }
 })
